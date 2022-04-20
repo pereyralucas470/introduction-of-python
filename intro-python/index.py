20ef3ad932edab3fbcd261a1d1f1878044cf65da
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2022-04-20 18:19:28 -0300

    introduction of python

---
+++ intro-python/index.py
@@ -0,0 +1,30 @@
+#BASIC MATHEMATICS
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+
+#LOGICAL OPERATORS
+# != Diferent
+# == Same
+# not Negation
+
+# CONDITIONAL SENTENCE "IF"
+if 3 > 5:
+    print('Esto no se va a imprimir')
+
+if 5 > 3:
+    print('5 es mayor a 3')
+
+# VARIABLES AND MULTIPLE VARIABLES
+my_name, age, nationality = 'Lucas', 19, 'Argentina'
+email = 'lucaspereyra470@gmail.com'
+
+print(my_name, age, nationality)
+print(email)
+
+# CONCATENATION
+start = 'Hello '
+final = 'world'
+
+print(start + final)
